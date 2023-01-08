feb99d902c60ba0471a13fd07d15ef5570d04e99
JoshQM <57198384+Nyxnix@users.noreply.github.com>
2023-01-07 19:11:19 -0500

    added error checking and clearing
    Error checking to make sure integers are in range for each option to prevent issues. Added menu option to "clear" the crontab.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PS3="Select an option: "
-select opt in local schedule info quit; do
+select opt in local schedule clear info quit; do
 
     case $opt in
       # Local transfers
@@ -22,6 +22,11 @@ select opt in local schedule info quit; do
         case $time in
          day)
          read -p "What time of day (hour only) would you like to backup on? (24 hour format, 0 - 23): " hour
+         # Check if the input is an integer
+         if ! [[ "$hour" =~ ^[0-9]+$ ]] || ! [[ "$hour" -ge 0 && "$hour" -le 23 ]]; then
+           echo "Error: Hour must be an integer between 0 and 23"
+           exit 1
+         fi
          # cron expression "0 $hour * * *"
          (crontab -l; echo "0 $hour * * * rsync -avu '$dir1' '$dir2'") | sort -u | crontab -
          break
@@ -29,6 +34,11 @@ select opt in local schedule info quit; do
          week)
          read -p "What day of the week would you like to backup? (0 - 6): " dow
          read -p "What time of day (hour only) would you like to backup on? (24 hour format, 0 - 23): " hour
+         # Check if the input is an integer
+         if ! [[ "$hour" =~ ^[0-9]+$ ]] || ! [[ "$hour" -ge 0 && "$hour" -le 23 ]] || ! [[ "$dow" =~ ^[0-9]+$ ]] || ! [[ "$dow" -ge 0 && "$dow" -le 6 ]]; then
+           echo "Error: Hour must be an integer between 0 and 23 and day of week must be an integer between 0 and 6"
+           exit 1
+         fi
          # cron expression "0 $hour * * $dow"
          (crontab -l; echo "0 $hour * * $dow rsync -avu '$dir1' '$dir2'") | sort -u | crontab -
          break
@@ -36,6 +46,10 @@ select opt in local schedule info quit; do
          month)
          read -p "What day of the month would you like to backup on? (1 - 31): " dom
          read -p "What time of day (hour only) would you like to backup on? (24 hour format, 0 - 23): " hour
+         if ! [[ "$dom" =~ ^[0-9]+$ ]] || ! [[ "$dom" -ge 1 && "$dom" -le 31 ]] || ! [[ "$hour" =~ ^[0-9]+$ ]] || ! [[ "$hour" -ge 0 && "$hour" -le 23 ]]; then
+           echo "Error: Day of month must be an integer between 1 and 31 and hour must be an integer between 0 and 23"
+           exit 1
+         fi
          # cron expression "0 $hour $dom * *"
          (crontab -l; echo "0 $hour $dom * * rsync -avu '$dir1' '$dir2'") | sort -u | crontab -
          break
@@ -54,6 +68,13 @@ select opt in local schedule info quit; do
       echo Simple Backup Tool
       echo By: Nyxnix
       echo  
+      break
+      ;;
+      clear)
+      read -p "Are you sure you want to continue? (y/n) " response
+      if [[ "$response" =~ ^[Yy]$ ]]; then
+      (crontab -r) # This completely deletes the crontab, I plan on making a better implementation eventually.
+      fi
       break
       ;;
       # Quit
